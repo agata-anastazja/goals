f655efd46a72e8618c2644d7d62bc28656b202bb
Agata Sumowska <agata.sumowska@gmail.com>
2023-02-01 10:03:40 +0000

    wip

--- test/goals/core_test.clj
+++ test/goals/core_test.clj
@@ -8,19 +8,19 @@
     [next.jdbc.result-set :as rs]
     [clojure.java.io :as io]))
 
-(deftest test-app
-  (testing "adding a yearly goal"
-    (migrate/migrate "jdbc:sqlite:goals-test.db")
-   (let [ds (jdbc/get-datasource {:jdbcUrl "jdbc:sqlite:goals-test.db"})
-          example-request {
-            :ds ds
-            :parameters {:body {:description "Feel pride in my work"
-                        :level "yearly"}}}
-        result  (core/add-goal example-request )
-        rows (jdbc/execute! ds ["select * from goals"] {:builder-fn rs/as-unqualified-lower-maps})
-        goal (first rows)]
-        (prn rows)
-        (is (= {:goal "Feel pride in my work" :goal_level "yearly"} goal))
+;; (deftest test-app
+;;   (testing "adding a yearly goal"
+;;     (migrate/migrate "jdbc:sqlite:goals-test.db")
+;;    (let [ds (jdbc/get-datasource {:jdbcUrl "jdbc:sqlite:goals-test.db"})
+;;           example-request {
+;;             :ds ds
+;;             :parameters {:body {:description "Feel pride in my work"
+;;                         :level "yearly"}}}
+;;         result  (core/add-goal example-request )
+;;         rows (jdbc/execute! ds ["select * from goals"] {:builder-fn rs/as-unqualified-lower-maps})
+;;         goal (first rows)]
+;;         (prn rows)
+;;         (is (= {:goal "Feel pride in my work" :goal_level "yearly"} goal))
         
-      (is (= 200 (:status result))))
-      (io/delete-file "goals-test.db")))
+;;       (is (= 200 (:status result))))
+;;       (io/delete-file "goals-test.db")))

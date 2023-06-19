65c5df571f1b06b356d2566dcab18f6eb1f75618
Agata Sumowska <agata.sumowska@gmail.com>
2023-06-19 09:55:41 +0100

    run integration test on a single transaction

--- test/goals/integration/users_test.clj
+++ test/goals/integration/users_test.clj
@@ -3,25 +3,30 @@
    [goals.users :as users]
    [clojure.test :refer :all]
    [next.jdbc :as jdbc]
+   [goals.migrate :as migrate] 
    [next.jdbc.result-set :as rs]))
 
 (deftest test-app
   (testing "create a user"
+    ;; explicitly open connection so that I can close it
     (let [uri "jdbc:postgresql://127.0.0.1:5432/goals?user=goals&password=goals"
-          ds (jdbc/get-datasource {:jdbcUrl uri})
+          _ (migrate/migrate uri)
+          ds (jdbc/get-datasource {:jdbcUrl uri :auto-commit false})]
 
-          result (users/add {:parameters
-                                  {:body {:username "Rahul"
-                                          :password "Secretpassword"}}
-                                  :ds ds})
-          rows (jdbc/execute! ds ["select * from users"] {:builder-fn rs/as-unqualified-lower-maps})
-          last-inserted-row (dissoc (last rows) :id)]
-      (is (= (:status result)  200))
-      (is (= last-inserted-row  {:username "Rahul"
-                                          :password "Secretpassword"}))))
-  (testing "only creates users with unique usernames"
+      (with-open [conn (jdbc/get-connection ds {:auto-commit false})]
+        (let [result (users/add {:parameters
+                                 {:body {:username "Rahul"
+                                         :password "Secretpassword"}}
+                                 :ds conn})
+              rows (jdbc/execute! conn ["select * from users"] {:builder-fn rs/as-unqualified-lower-maps})
+              last-inserted-row (dissoc (last rows) :id)]
+          (is (=  200 (:status result)))
+          (is (= last-inserted-row  {:username "Rahul"
+                                     :password "Secretpassword"})))
+        )))
+   #_(testing "only creates users with unique usernames"
     (let [uri "jdbc:postgresql://127.0.0.1:5432/goals?user=goals&password=goals"
-          ds (jdbc/get-datasource {:jdbcUrl uri})
+          ds (jdbc/get-datasource {:jdbcUrl uri :auto-commit false})
           result (users/add {:parameters
                                   {:body {:username "Rahul"
                                           :password "Secretpassword"}}
